efd26bed529a1e76a17b865a3cea4fdc8ad8bdc1
bsv-trinh-tran <trinhtran@bravesoft.com.vn>
2024-02-15 14:48:52 +0700

    no message

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,18 @@
-<script setup lang="ts"></script>
-
 <template>
-  <main>line mini app</main>
+    <main>line mini app</main>
 </template>
+<script setup lang="ts">
+import { watch, ref, nextTick, onMounted } from "vue";
+import { Html5Qrcode } from "html5-qrcode";
+import liff from "@line/liff";
+onMounted(async () => {
+    await liff.init({ liffId: "2003527394-rzmKd74p" }).catch((err) => {
+        console.log("LIFFの初期化失敗。\n" + err);
+    });
+    if (!liff.isLoggedIn()) {
+        await liff.login();
+    }
+    const x = await liff.getProfile();
+    console.log(x);
+});
+</script>
